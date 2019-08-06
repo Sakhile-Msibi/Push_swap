dfcdd7df0736edad5043062f51e0e9dce49a594e
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-06 07:30:44 +0200

    jump_table.c

--- srcs/jump_table.c
+++ srcs/jump_table.c
@@ -6,16 +6,16 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 16:31:04 by smsibi            #+#    #+#             */
-/*   Updated: 2019/08/05 16:32:54 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/08/06 07:30:25 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "actions.h"
 
-void	jt(uint8_t jump_index, t_frame *stacks)
+void	jt(uint8_t jump_index, t_stacks *stacks)
 {
-	static void	(*pf[])(t_frame *stacks) = {rrr, rra, rrb, rr, ra, rb, ss, \
+	static void	(*pf[])(t_stacks *stacks) = {rrr, rra, rrb, rr, ra, rb, ss, \
 		sa, sb, pa, pb};
 
 	if (jump_index < 12)
