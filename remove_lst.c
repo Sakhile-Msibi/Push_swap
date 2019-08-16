7811e74c62563cf4e9be03caa5a0042dd7fe0c05
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-16 08:42:34 +0200

    remove_lst.c

---
+++ remove_lst.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   remove_lst.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/05 13:14:35 by smsibi            #+#    #+#             */
+/*   Updated: 2019/08/06 07:59:46 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	remove_element(t_lst **head, t_lst *element)
+{
+	if ((*head)->next == *head)
+	{
+		free(*head);
+		*head = NULL;
+	}
+	else
+	{
+		element->prev->next = element->next;
+		element->next->prev = element->prev;
+		free(element);
+	}
+}
+
+void	remove_head(t_lst **head)
+{
+	t_lst *temp;
+
+	if (!(*head))
+		return ;
+	temp = (*head)->next;
+	if ((*head)->next == *head)
+	{
+		free(*head);
+		*head = NULL;
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		(*head)->next->prev = (*head)->prev;
+		free(*head);
+		*head = temp;
+	}
+}
