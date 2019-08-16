74bcefd5beb7d8f5dc108cf92c5307472a25f8cb
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-16 11:03:59 +0200

    actions2.c

--- actions2.c
+++ actions2.c
@@ -6,14 +6,14 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 08:28:55 by smsibi            #+#    #+#             */
-/*   Updated: 2019/08/06 10:04:36 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/08/16 11:03:45 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "actions.h"
 
-void	rotate_a(t_stacks *stacks)
+void	rotate_a(t_stack *stacks)
 {
 	if (stacks->a && stacks->a->next)
 	{
@@ -22,7 +22,7 @@ void	rotate_a(t_stacks *stacks)
 	}
 }
 
-void	rotate_b(t_stacks *stacks)
+void	rotate_b(t_stack *stacks)
 {
 	if (stacks->b && stacks->b->next)
 	{
@@ -31,7 +31,7 @@ void	rotate_b(t_stacks *stacks)
 	}
 }
 
-void	rotate_ab(t_stacks *stacks)
+void	rotate_ab(t_stack *stacks)
 {
 	rotate_a(stacks);
 	rotate_b(stacks);
