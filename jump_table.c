24113c8a11a93c0991343c00dcca311889a96886
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-16 11:07:16 +0200

    jump_table.c

--- jump_table.c
+++ jump_table.c
@@ -6,16 +6,16 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/05 16:31:04 by smsibi            #+#    #+#             */
-/*   Updated: 2019/08/16 10:16:28 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/08/16 11:07:06 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "actions.h"
 
-void	jt(uint8_t jump_index, t_stacks *stacks)
+void	jt(uint8_t jump_index, t_stack *stacks)
 {
-	static void	(*pf[])(t_stacks *stacks) =
+	static void	(*pf[])(t_stack *stacks) =
 	{reverse_rot_ab, reverse_rot_a, reverse_rot_b, rotate_ab, \
 		rotate_a, rotate_b, swap_ab, swap_a, swap_b, push_a, push_b};
 
